87d4acde1710fb316f5cd797679346ba2d81b8e7
Andrew <54002542+flashducke@users.noreply.github.com>
2021-03-09 21:38:19 +0500

    new test
    new test

---
+++ js/main.js
@@ -0,0 +1,69 @@
+document.addEventListener("DOMContentLoaded", function (event) {
+    //decor bg line
+    var p = document.querySelector('.decor-stripes path'),
+        offset = 2000;
+
+    var offsetMe = function() {
+        if(offset < 0) offset = 2000;
+        p.style.strokeDashoffset = offset;
+        offset--;
+
+        requestAnimationFrame(offsetMe);
+    }
+
+    offsetMe();
+
+
+    // modal
+    $('.modal').on('click', function (e) {
+        if (e.target.classList.contains('modal')) {
+            $('.modal.show').removeClass('show');
+            $('body,html').removeClass('modal-active');
+        }
+
+    });
+
+    // form
+    let $formSub = $('.modal .submit-btn');
+    let $checkList = $('.modal-checklist form > label');
+    $formSub.each(function () {
+        let $this = $(this);
+        let $form = $this.parent();
+
+        const formSubmit = () => {
+            $form.on('submit', function (sub) {
+                sub.preventDefault();
+                clickBtn();
+            })
+        }
+        formSubmit();
+
+        const clickBtn = () => {
+            $this.parent().parent('.modal-item').addClass('form-send')
+        }
+
+        $this.on('click', function () {
+            formSubmit();
+        })
+    })
+    $checkList.each(function () {
+        let $this = $(this);
+        $this.on('click', function () {
+            if ($this.find('input').is(':checked')) {
+                console.log($this.find('input').val());
+                $this.siblings('.submit-btn').click();
+            }
+        })
+    })
+
+});
+
+function openModal(id) {
+    $('.modal#' + id).addClass('show');
+    $('body,html').addClass('modal-active');
+}
+
+function closeModal() {
+    $('.modal.show').removeClass('show');
+    $('body,html').removeClass('modal-active');
+}
